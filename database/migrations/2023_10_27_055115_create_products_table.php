a350b72cee85a70a8d05f6c356e97e2c68ce5b1e
Misbachul2405 <vicki240593@gmail.com>
2023-11-11 19:19:15 +0700

    penambahan laravel fortify

--- database/migrations/2023_10_27_055115_create_products_table.php
+++ database/migrations/2023_10_27_055115_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->foreingId('lemari_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('lemari_id')->constrained()->cascadeOnDelete();
             $table->foreignId('supplier_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('slug');
