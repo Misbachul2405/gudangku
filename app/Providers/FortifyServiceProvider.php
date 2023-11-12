5fb3face76f640cc1d8e0051dd322fa945f21af8
Misbachul2405 <vicki240593@gmail.com>
2023-11-12 10:22:40 +0700

    Revert "installasi dan Konfigurasi laravel Fortify"
    This reverts commit 163d010e1e29af7f1ddc0099bea9c12aa8c2be1f.

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -45,18 +45,5 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
-         /**
-          * VIEW
-          */
-
-          //login
-          Fortify::loginView (function () {
-            return view ('auth.login');
-          });
-
-          //register
-          Fortify::registerView(function() {
-            return view('auth.register');
-          });
     }
 }
